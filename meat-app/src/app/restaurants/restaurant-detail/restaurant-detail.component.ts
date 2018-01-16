59c375c84cba6c094faca61c09ddb3f108f47690
Otávio Felipe do Prado <otavio.prado@matera.com>
2018-01-16 18:42:09 -0200

    incluindo busca de detalhes do restaurant

--- meat-app/src/app/restaurants/restaurant-detail/restaurant-detail.component.ts
+++ meat-app/src/app/restaurants/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-restaurant-detail',
@@ -6,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  restaurant: Restaurant;
+
+  constructor(private restaurantService: RestaurantsService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.restaurantService.restaurantById(this.activatedRoute.snapshot.params['id'])
+      .subscribe(restaurant => {
+        this.restaurant = restaurant;
+      });
   }
 
 }
