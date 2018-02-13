6cc8fc4f1caa13570ee001ae502c925c837b764d
Otávio Prado <otaviofelipedoprado@gmail.com>
2018-02-13 14:11:49 -0200

    utilizando HTTPS e typescript

--- meat-app/backend/server.ts
+++ meat-app/backend/server.ts
@@ -1,3 +1,8 @@
+import * as jsonServer from 'json-server';
+import { Express } from 'express';
+import * as fs from 'fs';
+import * as https from 'https';
+
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -10,8 +15,17 @@ server.use(middlewares)
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
 
+//
+
 // Use default router
 server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
+
+const options = {
+  cert: fs.readFileSync('./backend/keys/cert.pem'),
+  key: fs.readFileSync('./backend/keys/key.pem')
+}
+
+https.createServer(options, server).listen(3001, () => {
+  console.log('JSON Server is running on https://localhost:3001')
 })
+
